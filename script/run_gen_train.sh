a7011ddbb39b5ba394860de02e7acd9f55e73ea3
jiaqit <jiaqit@cs-airob-gpu01.cmpt.sfu.ca>
2023-02-12 19:50:24 -0800

    update map

---
+++ script/run_gen_train.sh
@@ -0,0 +1,1 @@
+parallel ./pp --agents ./map/scen-random/warehouse-10-20-10-2-1-random-1.scen --map map/scen-even/warehouse-10-20-10-2-1.map --agentNum 50 --insNum 1000 --ppRun 200 --sid {} ::: {1..25}
